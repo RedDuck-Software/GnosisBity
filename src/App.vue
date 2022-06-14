82ad26ea57814cad64be6af6ecc3aaff98dba9a2
Sorizen <anderteiker505@gmail.com>
2022-06-14 11:12:52 +0300

    gnosis-safe-transactions

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <template>
   <div id="app">
-    <h1>
-      Safe address:
-    </h1>
+    <h1>Safe address:</h1>
     <p>
       {{ safeInfo.safeAddress }}
     </p>
-    <p>
-      erf3CwQdPsiA6KrGbdbjhA
-    </p>
+    <p>erf3CwQdPsiA6KrGbdbjhA</p>
     <div>
-      <input v-model="bityApiKey">
-      <button @click="connectBity">
-        auth
-      </button>
+      <input v-model="bityApiKey" />
+      <button @click="connectBity">auth</button>
     </div>
-    <button @click="createOrder">
-      Order
-    </button>
+    <button v-if="!isOrderSectionHidden" @click="createOrder">Order</button>
   </div>
 </template>
 
 <script>
 import SafeAppsSDK from '@gnosis.pm/safe-apps-sdk';
-import { BityApiClient } from "@bity/api";
-import { Order, Owner } from "@bity/api/models";
+// import axios from 'axios';
+import { BityApiClient } from '@bity/api';
+import { Order, Owner } from '@bity/api/models';
+
+// const API_DEFAULT_LINK = 'https://exchange.api.bity.com';
 
 export default {
-  name: "App",
+  name: 'App',
 
   data() {
     return {
@@ -36,12 +31,12 @@ export default {
       bity: {},
       bityClientId: '',
       safeInfo: {},
-    }
+      isOrderSectionHidden: true,
+    };
   },
 
   async mounted() {
-    await this.loadSdk()
-    await this.connectBity()
+    await this.loadSdk();
   },
 
   methods: {
@@ -50,32 +45,39 @@ export default {
       try {
         this.safeInfo = await this.appsSdk.safe.getInfo();
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     },
 
-    async connectBity () {
+    async connectBity() {
       this.bity = await new BityApiClient({
-        exchangeApiUrl: 'https://exchange.api.bity.com/v2',
-        clientId: this.bityApiKey,
+        exchangeApiUrl: 'https://exchange.api.bity.com',
+        bookmarksApiUrl: this.bityApiKey,
       });
-      console.log(this.bity)
+
+      this.isOrderSectionHidden = false;
     },
 
     async createOrder() {
-      const ibanNum = 'CH0400766000103138557'
-      const order = new Order()
-      const result = order.setInput('CHF', '1.00')
-          .do((input) => input
-              .setOwner(new Owner())
-              .setIban(ibanNum))
-          .setOutput('ETH')
-          .do((output) => output
-              .setCryptoAddress(this.safeInfo.safeAddress));
+      const ibanNum = 'CH0400766000103138557';
+      const order = new Order();
+      const result = order
+        .setInput('CHF', '50.00')
+        .do((input) => input.setOwner(new Owner()).setIban(ibanNum))
+        .setOutput('ETH')
+        .do((output) =>
+          output.setCryptoAddress('0x8ED80CCF20F1E284eb56F2Ea225636F1aAC647Ce')
+        );
       const preparedOrder = await result.generateObjectForOrderCreation();
-      this.bity.createOrder(preparedOrder)
-    }
-  }
+      const res = await this.bity.createOrder(preparedOrder);
+      const paymentDetails = await this.bity.fetchOrderWithUrl(res);
+      console.log(paymentDetails, res);
+      // const link = API_DEFAULT_LINK + res;
+      // console.log(link);
+      // const { data } = await axios.get(link);
+      // console.log(data);
+    },
+  },
 };
 </script>
 
